e942a14a58b41e4f11cd6186faaed86d9c72baa6
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-05-03 12:49:56 +0330

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { GracefulShutdownServer } = require("medusa-core-utils");
 
 const loaders = require("@medusajs/medusa/dist/loaders/index").default;
-console.log("test");
+console.log("testf");
 (async () => {
   async function start() {
     const app = express();
